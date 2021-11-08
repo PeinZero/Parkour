57748ab6896e84f5c55a1fcf565dbb14fe345698
PeinZero <mahadzx@gmail.com>
2021-11-09 00:33:38 +0500

    map is being displayed

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import ParkerHome from './pages/Parker/ParkerHome';
+import MapContainer from './components/GMap';
 
 const App: React.FC = () => {
 
@@ -35,7 +36,7 @@ const App: React.FC = () => {
               <Redirect to="/page/Inbox" />
             </Route>
             <Route path= "/page/parkerhome"  exact={true}>
-              <ParkerHome />
+              <MapContainer />
             </Route>
           </IonRouterOutlet>
         </IonSplitPane>
