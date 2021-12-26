fd19cdc8a4d2bf492188e130f89893c6f84ffa53
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-12-26 18:31:21 +0500

    Update Login.tsx

--- src/pages/AuthPages/Login.tsx
+++ src/pages/AuthPages/Login.tsx
@@ -1,46 +1,54 @@
-import { Fragment } from 'react'
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { useAppDispatch } from '../../store/hooks';
-import {sendLoginData} from '../../store/Authentication/authenticationActions';
+import { useAppDispatch } from "../../store/hooks";
+import { sendLoginData } from "../../store/Authentication/authenticationActions";
 
-import styles from './Login.module.css'
+import styles from "./Login.module.css";
 
-import Button from '../../components/UI/Button/Button';
-import Input from '../../components/UI/Input/Input';
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 const Login: React.FC = () => {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const formSubmitHandler = (e) => {
-        e.preventDefault();
-        
-        dispatch(sendLoginData({
-            phone: e.target.phone.value,
-            password: e.target.password.value,
-        })) 
-    }
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
 
-    return (
-        <Fragment>
-            <form onSubmit = {formSubmitHandler} className={styles['form']}>
-                <Link to="/" className={styles['backLink']}>
-                    <ArrowBackIosNewIcon className={styles['back']} sx={{ fontSize: 36 }}/>
-                </Link>
-                <h1>Login</h1>
-                <Input label="Phone Number" name="phone" type="text" 
-                       placeholder="03158542543"
-                />
-                <Input label="Password" name="password" type="password" />
-                <Button className="primary">Login</Button>
-            </form>
-        </Fragment>
-    )
-}
+    dispatch(
+      sendLoginData({
+        phone: e.target.phone.value,
+        password: e.target.password.value,
+      })
+    );
+  };
+
+  return (
+    <Fragment>
+      <form onSubmit={formSubmitHandler} className={styles["form"]}>
+        <Link to="/" className={styles["backLink"]}>
+          <ArrowBackIosNewIcon
+            className={styles["back"]}
+            sx={{ fontSize: 36 }}
+          />
+        </Link>
+        <h1>Login</h1>
+        <Input
+          label="Phone Number"
+          name="phone"
+          type="text"
+          placeholder="03158542543"
+        />
+        <Input label="Password" name="password" type="password" />
+        <Button className="primary">Login</Button>
+      </form>
+    </Fragment>
+  );
+};
 
 // interface LoginProps {
 //     onLogin: (event: React.FormEvent<HTMLInputElement>, authData: {}) => void
 // }
 
-export default Login
+export default Login;
