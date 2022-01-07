8681c9a7424a8be45117eaa681ea11332efb4ff4
Abdullah Raheel <Abdullaharaheel@gmail.com>
2022-01-07 16:59:56 +0500

    s

--- src/store/User/userActions.tsx
+++ src/store/User/userActions.tsx
@@ -1,31 +1,26 @@
-import axios from "axios"
-import { userActions } from "./user"
+import axios from "axios";
+import { userActions } from "./user";
 
 export const fetchUser = (userId, token) => {
-    return async (dispatch) => {
-        
-        const sendRequest = async () => {
-            const response = await axios.get(
-                `http://localhost:5000/user/getUser/${userId}`,
-                {
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    }
-                }
-            )
-            
-            return response;
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.get(
+        `${process.env.bakendLink}/user/getUser/${userId}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
         }
-        
-        try {
-            const response = await sendRequest();
-            dispatch(userActions.createUser(response.data.user))
-            
-        } catch (error) {
-            console.log('Failed to fetch user date!')
-        }
-       
+      );
+
+      return response;
+    };
+
+    try {
+      const response = await sendRequest();
+      dispatch(userActions.createUser(response.data.user));
+    } catch (error) {
+      console.log("Failed to fetch user date!");
     }
-}
-
-
+  };
+};
