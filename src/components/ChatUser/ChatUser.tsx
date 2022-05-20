fcbad4ac3cb52dd8f5b2a630331896d4125a64c9
Abdullah Raheel <Abdullaharaheel@gmail.com>
2022-05-21 02:57:31 +0500

    User Chat

--- src/components/ChatUser/ChatUser.tsx
+++ src/components/ChatUser/ChatUser.tsx
@@ -1,7 +1,85 @@
+// React Imports
 import React from "react";
+import { useLocation } from "react-router-dom";
+
+// Component Imports
+import Header from "../UI/Header/Header";
+
+// Style / Icon Imports
+import styles from "./ChatUser.module.css";
+
+let allMessages = [
+  {
+    id: 1,
+    sender: "You",
+    message: "Hey, how are you?",
+  },
+  {
+    id: 2,
+    sender: "Mahad Khalid",
+    message: "I didn't recieve the payment",
+  },
+  {
+    id: 3,
+    sender: "You",
+    message: "You are not here yet",
+  },
+  {
+    id: 4,
+    sender: "You",
+    message: "When will you come ?",
+  },
+  {
+    id: 5,
+    sender: "Mahad Khalid",
+    message: "Yup the Spot is free !",
+  },
+];
 
 const ChatUser = () => {
-  return <div>ChatUser</div>;
+  const location = useLocation();
+  const { state } = location;
+  let chatInfo: any;
+  const locationState: any = state;
+
+  if (locationState) {
+    chatInfo = locationState.chat;
+  }
+
+  let chatList = allMessages.map((msg) => {
+    if (msg.sender === "You") {
+      return (
+        <div className={`${styles["chat-msg"]} ${styles["sent"]}`} key={msg.id}>
+          {msg.message}
+        </div>
+      );
+    } else {
+      return (
+        <div className={`${styles["chat-msg"]} ${styles["received"]}`} key={msg.id}>
+          {msg.message}
+        </div>
+      );
+    }
+  });
+
+  return (
+    <div className={styles["chat-container"]}>
+      <Header backLink="/" content="Chat" />
+
+      <div className={styles["chat-header"]}>
+        {/* <div className={styles["chat-header-image"]}> */}
+        <img src={chatInfo.imgSrc} alt="" />
+        {/* </div> */}
+        <h3>{chatInfo.name}</h3>
+      </div>
+
+      <div className={styles["chat-content"]}>{chatList}</div>
+
+      <div className={styles["chat-input"]}>
+        <input type="text" placeholder="send message" />
+      </div>
+    </div>
+  );
 };
 
 export default ChatUser;
--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,5 +1,11 @@
+// React Imports
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+// Component Imports
 import Header from "../../components/UI/Header/Header";
+
+// Style / Icon Imports
 import styles from "./Chat.module.css";
 
 let recentlyChatted = [
@@ -7,51 +13,60 @@ let recentlyChatted = [
     id: 1,
     name: "Abdullah Raheel",
     lastMessage: "Hey, how are you?",
+    imgSrc: "/images/mahad_profile_pic.jpg",
   },
   {
     id: 2,
     name: "Mahad Khalid",
     lastMessage: "I didn't recieve the payment",
+    imgSrc: "/images/mahad_profile_pic.jpg",
   },
   {
     id: 3,
     name: "Ammar Nasir",
     lastMessage: "You are not here yet",
+    imgSrc: "/images/mahad_profile_pic.jpg",
   },
   {
     id: 4,
     name: "Elusive",
     lastMessage: "When will you come ?",
+    imgSrc: "/images/mahad_profile_pic.jpg",
   },
   {
     id: 5,
     name: "John Doe",
     lastMessage: "Yup the Spot is free !",
+    imgSrc: "/images/mahad_profile_pic.jpg",
   },
 ];
 
-const chatClickHander = (id) => {
-  console.log(id);
-};
-
 const Chat = () => {
+  const navigate = useNavigate();
+
   let chatList = recentlyChatted.map((chat) => {
     return (
       <div
         className={styles["chat-list-item"]}
         key={chat.id}
-        onClick={() => chatClickHander(chat.id)}>
+        onClick={() => chatClickHander(chat)}>
         <div className={styles["chat-list-image"]}>
-          <img src="/images/mahad_profile_pic.jpg" alt="" />
+          <img src={chat.imgSrc} alt="" />
         </div>
         <div className={styles["chat-text"]}>
           <h3>{chat.name}</h3>
-
           <p>{chat.lastMessage}</p>
         </div>
       </div>
     );
   });
+
+  const chatClickHander = (chat: any) => {
+    console.log(chat);
+    navigate(`/chat/${chat.id}`, {
+      state: { chat },
+    });
+  };
 
   return (
     <div className={styles["chat-container"]}>
